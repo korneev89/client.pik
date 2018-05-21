6cba918b1a903d3474b61503b9551c0ef0cb8397
Dmitry Korneev <31965644+korneev89@users.noreply.github.com>
2018-05-21 16:13:37 +0300

    worked version

--- client.pik/Program.cs
+++ client.pik/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Net;
 using System.Reflection;
+using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -21,79 +24,178 @@ namespace client.pik
 			ChromeOptions options = new ChromeOptions();
 			options.AddArgument("headless");
 			driver = new ChromeDriver(options);
-			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+			driver.Manage().Window.Size = new System.Drawing.Size(1500, 900);
 		}
 
 		[Test]
 		public void CheckNews()
 		{
-			Login();
-			string flatNewsId = "";
-
-			var textFileInfo =
-				new FileInfo(Path.Combine(
-					Path.GetDirectoryName(
-						Assembly.GetExecutingAssembly().Location),
-						@"news_id.txt"));
-
-			if (!textFileInfo.Exists)
-			{
-				using (var f = File.Create(textFileInfo.FullName)) { }
-			}
-			
-			var newsIdString = File.ReadAllText(textFileInfo.FullName);
-
-			var FlatNewsLink = "https://client.pik.ru/object/c8ac67d6-9831-e711-857e-001ec9d56418/news";
-			var pantryNewsLink = "https://client.pik.ru/object/d61206ad-e721-e811-b0fc-0050568859fb/news";
-
-			try
-			{
-				driver.Url = FlatNewsLink;
-				WaitForNewsPageLoad();
-				var newsFlatLinkParts = driver.FindElements(By.CssSelector("a.News-list--link"))[0].GetAttribute("href").Split('/');
-				flatNewsId = newsFlatLinkParts[newsFlatLinkParts.Length - 1];
-				Assert.AreEqual(newsIdString, flatNewsId, "по квартире");
-
-
-				driver.Url = pantryNewsLink;
-				WaitForNewsPageLoad();
-				var newsPantryLinkParts = driver.FindElements(By.CssSelector("a.News-list--link"))[0].GetAttribute("href").Split('/');
-				var pantryNewsId = newsPantryLinkParts[newsPantryLinkParts.Length - 1];
-				Assert.AreEqual(newsIdString, pantryNewsId, "по кладовке");
-			}
-			catch (AssertionException e)
-			{
-				var message = $"В личном кабинете ПИК есть свежие новости! {FlatNewsLink}";
-				SendTelegram(message);
-
-				using (var f = File.Create(textFileInfo.FullName)){}
-				File.WriteAllText(textFileInfo.FullName, flatNewsId);
-			}
-			catch (Exception e)
-			{
-				var message = $"Дружок, твой тест упал :( Смотри ошибку: {e.Message}";
-				SendTelegram(message);
-			}
-
+			var users = CreateUsers();
 			var today = DateTime.Now;
-			if (today.Hour == 17 && (today.Minute == 0 || today.Minute == 1))
+
+			foreach (var user in users)
 			{
-				var message = "В личном кабинете ПИК нет свежих новостей";
-				SendTelegram(message);
+				Login(user);
+				string flatNewsId = "";
+
+				var txtName = $"news_id_{user.Name}.txt";
+				FileInfo textFileInfo = CreateFileInfoToAssemblyDirectory(txtName);
+
+				if (!textFileInfo.Exists)
+				{
+					using (var f = File.Create(textFileInfo.FullName)) { }
+				}
+
+				var logName = "log.txt";
+				FileInfo logFileInfo = CreateFileInfoToAssemblyDirectory(logName);
+
+				if (!logFileInfo.Exists)
+				{
+					using (var f = File.Create(logFileInfo.FullName)) { }
+				}
+
+				var logsCountName = "logs_count.txt";
+				FileInfo logsCountFileInfo = CreateFileInfoToAssemblyDirectory(logsCountName);
+
+				if (!logsCountFileInfo.Exists)
+				{
+					using (var f = File.Create(logsCountFileInfo.FullName)) { };
+					File.WriteAllText(logsCountFileInfo.FullName, "0");
+				}
+
+				var continErrorsName = "errors_conti.txt";
+				FileInfo continErrorsInfo = CreateFileInfoToAssemblyDirectory(continErrorsName);
+
+				if (!continErrorsInfo.Exists)
+				{
+					using (var f = File.Create(continErrorsInfo.FullName)) { };
+					File.WriteAllText(continErrorsInfo.FullName, "0");
+				}
+
+				try
+				{
+					if (user.FlatGuid != null)
+					{
+						var newsIdString = File.ReadAllText(textFileInfo.FullName);
+
+						driver.Url = $"https://client.pik.ru/object/{user.FlatGuid}/news";
+						WaitForNewsPageLoad();
+						var newsFlatLinkParts = driver.FindElements(By.CssSelector("a.News-list--link"))[0].GetAttribute("href").Split('/');
+						flatNewsId = newsFlatLinkParts[newsFlatLinkParts.Length - 1];
+
+						if (newsIdString != flatNewsId)
+						{
+							var message = $"В личном кабинете ПИК есть свежие новости! https://client.pik.ru/object/{user.FlatGuid}/news";
+							SendTelegram(user, message);
+
+							File.WriteAllText(textFileInfo.FullName, flatNewsId);
+						}
+
+						driver.Url = $"https://client.pik.ru/object/{user.FlatGuid}/info";
+						wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector(".PropertiesList-blockName")));
+						wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("button")));
+
+						Thread.Sleep(10000);
+						var buttonsCount = driver.FindElements(By.CssSelector("button")).Count;
+
+						if (buttonsCount != 13)
+						{
+							var message = $"В личном кабинете ПИК изменилось количество кнопок, ломись туда! https://client.pik.ru/object/{user.FlatGuid}/news";
+							SendTelegram(user, message);
+						}
+					}
+
+					if (user.PantryGuid != null)
+					{
+						var newsIdString = File.ReadAllText(textFileInfo.FullName);
+
+						driver.Url = $"https://client.pik.ru/object/{user.PantryGuid}/news";
+						WaitForNewsPageLoad();
+						var newsPantryLinkParts = driver.FindElements(By.CssSelector("a.News-list--link"))[0].GetAttribute("href").Split('/');
+						var pantryNewsId = newsPantryLinkParts[newsPantryLinkParts.Length - 1];
+
+						if (newsIdString != pantryNewsId)
+						{
+							var message = $"В личном кабинете ПИК есть свежие новости! https://client.pik.ru/object/{user.PantryGuid}/news";
+							SendTelegram(user, message);
+
+							File.WriteAllText(textFileInfo.FullName, flatNewsId);
+						}
+					}
+
+					Logout();
+
+					if (today.Hour == 17 && (today.Minute == 0))
+					{
+						var message = "В личном кабинете ПИК нет свежих новостей";
+						SendTelegram(user, message);
+					}
+
+					File.WriteAllText(continErrorsInfo.FullName, "0");
+				}
+
+				catch (Exception e)
+				{
+					var logsCount = Int32.Parse(File.ReadAllText(logsCountFileInfo.FullName));
+					logsCount++;
+
+					File.WriteAllText(logsCountFileInfo.FullName, logsCount.ToString());
+
+					File.AppendAllText(logFileInfo.FullName, Environment.NewLine + $"{today} - {e.Message}");
+
+					var contiErrorsCount = Int32.Parse(File.ReadAllText(continErrorsInfo.FullName));
+					contiErrorsCount++;
+
+					if (contiErrorsCount > 9)
+					{
+						SendTelegramToAdmin($"Тест зафейлился {contiErrorsCount} раз подряд!!! Нужно что-то сделать");
+						SendTelegramToAdmin($"Общее количество ошибок - {logsCount}, смотри файл лога");
+						contiErrorsCount = 0;
+					}
+
+					File.WriteAllText(continErrorsInfo.FullName, contiErrorsCount.ToString());
+				}
 			}
 
-			var mess = "В личном кабинете ПИК нет свежих новостей";
-			Assert.Pass(mess);
 
+			//var mess = "Тест прошёл успешно";
+			//Assert.Pass(mess);
 		}
 
-		private void SendTelegram(string message)
+		private static FileInfo CreateFileInfoToAssemblyDirectory(string name)
+		{
+			return new FileInfo(Path.Combine(
+					Path.GetDirectoryName(
+						Assembly.GetExecutingAssembly().Location),
+						name));
+		}
+
+		private void Logout()
+		{
+			wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector(".Header-logoutButton")));
+			driver.FindElement(By.CssSelector(".Header-logoutButton")).Click();
+			wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("input#login")));
+		}
+
+		public static List<User> CreateUsers()
+		{
+			var users = new List<User>();
+
+
+
+			return users;
+		}
+
+		private void SendTelegram(User user, string message)
 		{
 			var telegramURL = @"https://api.telegram.org";
-			var token = "";
-			var chat_id = "";
+			var token = ConfigurationManager.AppSettings["bot_token"];
+			var chat_id = user.ChatId;
 			string url = $"{telegramURL}/bot{token}/sendMessage?chat_id={chat_id}&text={message}";
 			driver.Url = url;
+			wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("body > pre")));
+			driver.Url = "https://client.pik.ru/object";
 
 			/* using proxy
 			 
@@ -109,15 +211,24 @@ namespace client.pik
 			*/
 		}
 
+		private void SendTelegramToAdmin(string message)
+		{
+			var telegramURL = @"https://api.telegram.org";
+			var token = ConfigurationManager.AppSettings["bot_token"];
+			var chat_id = "168694373";
+			string url = $"{telegramURL}/bot{token}/sendMessage?chat_id={chat_id}&text={message}";
+			driver.Url = url;
+		}
+
 		private void WaitForNewsPageLoad()
 		{
 			wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("a.News-list--link")));
 		}
 
-		private void Login()
+		private void Login(User user)
 		{
-			var login = "";
-			var pass = "";
+			var login = user.Login;
+			var pass = user.Password;
 
 			if (login == "" || pass == "") { throw new System.ArgumentException("Please provide correct login data"); }
 
@@ -128,7 +239,7 @@ namespace client.pik
 			var oldPage = driver.FindElement(By.CssSelector("div.Page"));
 
 			driver.FindElement(By.CssSelector("button.Button.Button-marginBottom")).Click();
-			wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector(".DropdownLink-content")));
+			wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector(".Header-account--text")));
 		}
 
 		[TearDown]
---
+++ client.pik/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.pik
+{
+	public class User
+	{
+		public string Name;
+		public string Login;
+		public string Password;
+		public string ChatId;
+		public string FlatGuid;
+		public string PantryGuid;
+	}
+}
